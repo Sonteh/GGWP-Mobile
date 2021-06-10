a9e82d7f16147f5c3fee5b5bc73dfd63b1eee581
Omik Futerkowski <dawidszab@wp.pl>
2021-06-10 21:20:29 +0200

    Showing basic information about searched summoner

--- app/src/main/java/com/example/ggwp_mobile/SearchScreenFragment.kt
+++ app/src/main/java/com/example/ggwp_mobile/SearchScreenFragment.kt
@@ -4,16 +4,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_search_screen.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.Dispatchers.Main
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import org.json.JSONObject
 import java.net.URL
-import kotlinx.android.synthetic.main.fragment_search_screen_.*
 
 
 class SearchScreenFragment : Fragment() {
@@ -27,9 +29,9 @@ class SearchScreenFragment : Fragment() {
         savedInstanceState: Bundle?
 
     ): View? {
-        val layout = inflater.inflate(R.layout.fragment_search_screen_, container, false)
-        val editSummoner: EditText = layout.findViewById(R.id.editTextSummonerName)
-        val textSummoner: TextView = layout.findViewById(R.id.textView)
+        val layout = inflater.inflate(R.layout.fragment_search_screen, container, false)
+        //val editSummoner: EditText = layout.findViewById(R.id.)
+        //val textSummoner: TextView = layout.findViewById(R.id.textView)
         var apiResponse: String = "sd"
 //        var apiResponse: String
 //        thread {
@@ -54,11 +56,23 @@ class SearchScreenFragment : Fragment() {
 //        sss.runOnUiThread(Runnable {
 //            // change UI elements here
 //        })
+        val buttonCalories = layout.findViewById<Button>(R.id.button)
 
-        CoroutineScope(IO).launch {
-            fakeSummoner()
+        buttonCalories.setOnClickListener {
+            val Summoner = editTextTextPersonName!!.text.toString()
+            println(Summoner)
+
+            //Start IO (input/output) coroutine for network operations
+            CoroutineScope(IO).launch {
+                fakeSummoner(Summoner)
+            }
         }
-        //println("hmmmmmmmmmmmmmmmmmmmmmmmmmmmm$apiResponse")
+
+
+//        CoroutineScope(IO).launch {
+//            fakeSummoner()
+//        }
+
 
         return layout
     }
@@ -68,28 +82,52 @@ class SearchScreenFragment : Fragment() {
 //        text.textView = apiResponse
 //        layoutInflater.textView
         println("\n$input")
-        textView.text = "\n$input"
+        textView.text = input
     }
 
-    private suspend fun setTextOnMainThread(input: String){
+    private fun setNewText2(input: String){
+        println("\n$input")
+        textView2.text = input
+    }
+
+    private suspend fun setTextOnMainThread(input: String, input2: String){
+        //Start Main coroutine for operations on UI
         withContext(Main){
             setNewText(input)
+            setNewText2("Global mastery score: $input2")
         }
     }
 
-    private suspend fun fakeSummoner(){
-        val apiResponse = getSummoner()
-        setTextOnMainThread(apiResponse)
+    private suspend fun fakeSummoner(input: String): String{
+        val apiResponse = getSummoner(input)
+
+        val json = JSONObject(apiResponse) // String instance holding the above json
+        val summonerId = json.getString("id")
+        println("summoner id hereeeeeeeeeeeeeeeeeeeeee $summonerId")
+
+        val masteryScore = getMasteryScore(summonerId)
+
+        setTextOnMainThread(apiResponse, masteryScore)
+
+
+
+        return summonerId //testing here
     }
 
-    private suspend fun getSummoner(): String {
+    private suspend fun getSummoner(input: String): String {
         logThread("getSummoner")
         //val apiResponse = URL("https://eun1.api.riotgames.com/lol/summoner/v4/summoners/by-name/Davidoski?api_key=RGAPI-d514c47e-e06f-4d8c-bd8a-503fef034e21").readText()
-        return URL("https://eun1.api.riotgames.com/lol/summoner/v4/summoners/by-name/Davidoski?api_key=RGAPI-d514c47e-e06f-4d8c-bd8a-503fef034e21").readText()
+        return URL("https://eun1.api.riotgames.com/lol/summoner/v4/summoners/by-name/$input?api_key=RGAPI-d514c47e-e06f-4d8c-bd8a-503fef034e21").readText()
         //setTextOnMainThread(apiResponse)
     }
 
+    private suspend fun getMasteryScore(input: String): String {
+        return URL("https://eun1.api.riotgames.com/lol/champion-mastery/v4/scores/by-summoner/$input?api_key=RGAPI-d514c47e-e06f-4d8c-bd8a-503fef034e21").readText()
+    }
+
+    //function to log current thread where operation is taking place
+    //Can be deleted later
     private fun logThread(methodName: String) {
-        //println("debug: ${methodName}: ${Thread.currentThread().name}")
+        println("debug: ${methodName}: ${Thread.currentThread().name}")
     }
 }
