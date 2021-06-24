a8a09558dbdece2a8b7c267fac5ae755f49d6536
Gracjan Redwanc <redwanc.gracjan@gmail.com>
2021-06-24 18:57:17 +0200

    Changed match history to Card View with button listeners to expand information

--- app/src/main/java/com/example/ggwp_mobile/MatchHistoryFragment.kt
+++ app/src/main/java/com/example/ggwp_mobile/MatchHistoryFragment.kt
@@ -1,16 +1,19 @@
 package com.example.ggwp_mobile
 
 import android.annotation.SuppressLint
+import android.app.Application
 import android.graphics.Bitmap
 import android.graphics.Color
 import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.transition.AutoTransition
+import android.transition.TransitionManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.LinearLayout
-import android.widget.TextView
+import android.widget.*
+import androidx.cardview.widget.CardView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import com.squareup.picasso.Picasso
@@ -40,13 +43,16 @@ class MatchHistoryFragment : Fragment() {
         val key = viewModel.returnKey()
         val summonerName2 = viewModel.returnSummonerName()
 
+//        val cardView = layout.findViewById<CardView>(R.id.base_cardview)
+//        val arrow = layout.findViewById<Button>(R.id.arrow_button)
+//        val hiddenView = layout.findViewById<View>(R.id.hidden_view)
+
         val linearLayout: LinearLayout = layout.findViewById(R.id.layout_main)
 
         CoroutineScope(Dispatchers.IO).launch {
             val matchHistory = getMatchHistory(key, puuid)
 
-            for (i in 0..19)
-            {
+            for (i in 0..19) {
                 val matchDetails = getMatchDetails(key, matchHistory.getString(i))
                 val json = JSONObject(matchDetails)
                 //val gameInfo = json.getString("info")
@@ -54,13 +60,11 @@ class MatchHistoryFragment : Fragment() {
 
                 val participants = gameInfo.getJSONArray("participants")
 
-                for(j in 0..9)
-                {
+                for (j in 0..9) {
                     val participant = participants.getJSONObject(j)
                     val summonerName = participant.get("summonerName")
 
-                    if (summonerName == summonerName2)
-                    {
+                    if (summonerName == summonerName2) {
                         val championName = participant.get("championName")
                         val championId = participant.get("championId")
                         val kills = participant.get("kills")
@@ -72,33 +76,45 @@ class MatchHistoryFragment : Fragment() {
                         withContext(Main) {
                             val view: View = layoutInflater.inflate(R.layout.match_child, null)
                             val matchItem: TextView = view.findViewById(R.id.match_item)
+                            val cardView: CardView = view.findViewById(R.id.base_cardview)
+                            val arrow: ImageButton = view.findViewById(R.id.arrow_button)
+                            val hiddenView: View = view.findViewById(R.id.hidden_view)
 
 
-                            Picasso.get().load("https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/champion-icons/$championId.png").into(object: com.squareup.picasso.Target {
+                            Picasso.get()
+                                .load("https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/champion-icons/$championId.png")
+                                .into(object : com.squareup.picasso.Target {
 
-                                override fun onBitmapLoaded(bitmap: Bitmap?, from: Picasso.LoadedFrom?)
-                                {
-                                    val drawImage: Drawable = BitmapDrawable(resources, bitmap)
-                                    matchItem.setCompoundDrawablesWithIntrinsicBounds(drawImage, null, null, null)
-                                }
+                                    override fun onBitmapLoaded(
+                                        bitmap: Bitmap?,
+                                        from: Picasso.LoadedFrom?
+                                    ) {
+                                        val drawImage: Drawable = BitmapDrawable(resources, bitmap)
+                                        matchItem.setCompoundDrawablesWithIntrinsicBounds(
+                                            drawImage,
+                                            null,
+                                            null,
+                                            null
+                                        )
+                                    }
 
-                                override fun onPrepareLoad(placeHolderDrawable: Drawable?)
-                                {}
+                                    override fun onPrepareLoad(placeHolderDrawable: Drawable?) {}
 
-                                override fun onBitmapFailed(e: Exception?, errorDrawable: Drawable?)
-                                {}
-                            })
-                            if (playerWin)
-                            {
+                                    override fun onBitmapFailed(
+                                        e: Exception?,
+                                        errorDrawable: Drawable?
+                                    ) {
+                                    }
+                                })
+                            if (playerWin) {
                                 playerResult = "WIN"
                                 matchItem.setBackgroundColor(Color.parseColor("#446cff"))
-                            }
-                            else
-                            {
+                            } else {
                                 playerResult = "LOST"
                                 matchItem.setBackgroundColor(Color.parseColor("#ff8385"))
                             }
                             matchItem.text = "$summonerName played $championName $kills/$deaths/$assists \n $playerResult"
+                            arrow.setOnClickListener(getOnClick(cardView, hiddenView))
                             linearLayout.addView(view)
                         }
                         println("$summonerName played $championName")
@@ -106,8 +122,41 @@ class MatchHistoryFragment : Fragment() {
                 }
             }
         }
-
         return layout
+    }
+
+    private fun getOnClick(cardView: CardView, hiddenView: View): View.OnClickListener
+    {
+        return View.OnClickListener {
+            println("TEST")
+            // If the CardView is already expanded, set its visibility
+            //  to gone and change the expand less icon to expand more.
+            if (hiddenView.visibility == View.VISIBLE) {
+
+                // The transition of the hiddenView is carried out
+                //  by the TransitionManager class.
+                // Here we use an object of the AutoTransition
+                // Class to create a default transition.
+                TransitionManager.beginDelayedTransition(
+                    cardView,
+                    AutoTransition()
+                );
+                hiddenView.visibility = View.GONE;
+                // arrow.setImageResource(R.drawable.ic_baseline_expand_more_24);
+            }
+
+            // If the CardView is not expanded, set its visibility
+            // to visible and change the expand more icon to expand less.
+            else {
+
+                TransitionManager.beginDelayedTransition(
+                    cardView,
+                    AutoTransition()
+                );
+                hiddenView.visibility = View.VISIBLE;
+                // arrow.setImageResource(R.drawable.ic_baseline_expand_less_24);
+            }
+        }
     }
 
     private fun getMatchHistory(apiKey: String, puuid: String): JSONArray {
