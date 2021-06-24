6b593d8522bf3d57314e4dd27d7d1d567eed2156
Omik Futerkowski <dawidszab@wp.pl>
2021-06-24 23:54:11 +0200

    Pie chart changed to bar chart

--- app/src/main/java/com/example/ggwp_mobile/ChartScreenFragment.kt
+++ app/src/main/java/com/example/ggwp_mobile/ChartScreenFragment.kt
@@ -9,10 +9,9 @@ import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.findNavController
+import com.github.mikephil.charting.charts.HorizontalBarChart
 import com.github.mikephil.charting.charts.PieChart
-import com.github.mikephil.charting.data.PieData
-import com.github.mikephil.charting.data.PieDataSet
-import com.github.mikephil.charting.data.PieEntry
+import com.github.mikephil.charting.data.*
 import com.github.mikephil.charting.utils.ColorTemplate
 import kotlinx.android.synthetic.main.fragment_chart_screen.*
 import kotlinx.coroutines.CoroutineScope
@@ -43,11 +42,18 @@ class ChartScreenFragment: Fragment() {
             val statsMap = fakeStats(summonerId, key)
             withContext(Main){
                 statsMap.get(64)
-                val pieChart = layout.findViewById<PieChart>(R.id.pieChart)
-                val visitiors: MutableList<PieEntry> = ArrayList()
+                val barChart = layout.findViewById<HorizontalBarChart>(R.id.horizontalChart)
+                val visitiors: MutableList<BarEntry> = ArrayList()
+                var i = 1
                 for ((key, value) in statsMap){
-                    visitiors.add(PieEntry(value.toFloat(), key))
+
+                    visitiors.add(BarEntry(i.toFloat(), value.toFloat()))
+                    i++
                 }
+//                visitiors.add(BarEntry(0f, 32185302f))
+//                visitiors.add(BarEntry(1f, 16263695f))
+//                visitiors.add(BarEntry(2f, 3180810f))
+//                visitiors.add(BarEntry(3f, 2751632f))
 //                visitiors.add(PieEntry(statsMap.get(64)!!.toFloat(), "Gibraltar"))
 //                visitiors.add(PieEntry(2710f, "Czech"))
 //                visitiors.add(PieEntry(1739f, "USA"))
@@ -58,17 +64,17 @@ class ChartScreenFragment: Fragment() {
 //                visitiors.add(PieEntry(920f, "Greece"))
 //                visitiors.add(PieEntry(732f, "Russia"))
 
-                val pieDataSet = PieDataSet(visitiors, "Covid infections per 1 million")
-                pieDataSet.setColors(*ColorTemplate.COLORFUL_COLORS)
-                pieDataSet.valueTextColor = Color.BLACK
-                pieDataSet.valueTextSize = 16f
+                val barDataSet = BarDataSet(visitiors, "Deaths in Poland every year")
+                barDataSet.setColors(*ColorTemplate.COLORFUL_COLORS)
+                barDataSet.valueTextColor = Color.BLACK
+                barDataSet.valueTextSize = 4f
 
-                val pieData = PieData(pieDataSet)
-
-                pieChart.data = pieData
-                pieChart.description.isEnabled = false
-                pieChart.centerText = "COVID"
-                pieChart.animate()
+                val barData = BarData(barDataSet)
+//
+                barChart.setFitBars(true)
+                barChart.data = barData
+                barChart.description.text = "Deaths"
+                barChart.animateY(2000)
             }
         }
 
@@ -109,7 +115,7 @@ class ChartScreenFragment: Fragment() {
         val champ = statsJsonObject.getString("championId")
         //print("sSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS$champ")
         var myMap = HashMap<Int, Int>()
-        for (j in 0..100){
+        for (j in 0..15){
             val statsToIterate = statsJson.getJSONObject(j)
             println(statsToIterate.getString("championPoints"))
             val key = statsToIterate.getString("championId").toInt()
@@ -118,8 +124,6 @@ class ChartScreenFragment: Fragment() {
         }
         print("mapałęłęęłęłę"+myMap)
         //setTextOnMainThread(summonerInfo, masteryScore, summonerIcon, summonerLvl, tier)
-
-        drawGraph()
         //print(stats)
         return myMap
     }
@@ -129,31 +133,31 @@ class ChartScreenFragment: Fragment() {
         return URL("https://eun1.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/$summonerId?api_key=$apiKey").readText()
     }
 
-    private fun drawGraph(){
-        //val pieChart = layout.findViewById<PieChart>(R.id.pieChart)
-        val pieChart = pieChart
-
-        val visitiors: MutableList<PieEntry> = ArrayList()
-        visitiors.add(PieEntry(2789f, "Gibraltar"))
-        visitiors.add(PieEntry(2710f, "Czech"))
-        visitiors.add(PieEntry(1739f, "USA"))
-        visitiors.add(PieEntry(1704f, "Poland"))
-        visitiors.add(PieEntry(1653f, "Spain"))
-        visitiors.add(PieEntry(1528f, "France"))
-        visitiors.add(PieEntry(982f, "Germany"))
-        visitiors.add(PieEntry(920f, "Greece"))
-        visitiors.add(PieEntry(732f, "Russia"))
-
-        val pieDataSet = PieDataSet(visitiors, "Covid infections per 1 million")
-        pieDataSet.setColors(*ColorTemplate.COLORFUL_COLORS)
-        pieDataSet.valueTextColor = Color.BLACK
-        pieDataSet.valueTextSize = 16f
-
-        val pieData = PieData(pieDataSet)
-
-        pieChart.data = pieData
-        pieChart.description.isEnabled = false
-        pieChart.centerText = "COVID"
-        pieChart.animate()
-    }
+//    private fun drawGraph(){
+//        //val pieChart = layout.findViewById<PieChart>(R.id.pieChart)
+//        val pieChart = pieChart
+//
+//        val visitiors: MutableList<PieEntry> = ArrayList()
+//        visitiors.add(PieEntry(2789f, "Gibraltar"))
+//        visitiors.add(PieEntry(2710f, "Czech"))
+//        visitiors.add(PieEntry(1739f, "USA"))
+//        visitiors.add(PieEntry(1704f, "Poland"))
+//        visitiors.add(PieEntry(1653f, "Spain"))
+//        visitiors.add(PieEntry(1528f, "France"))
+//        visitiors.add(PieEntry(982f, "Germany"))
+//        visitiors.add(PieEntry(920f, "Greece"))
+//        visitiors.add(PieEntry(732f, "Russia"))
+//
+//        val pieDataSet = PieDataSet(visitiors, "Covid infections per 1 million")
+//        pieDataSet.setColors(*ColorTemplate.COLORFUL_COLORS)
+//        pieDataSet.valueTextColor = Color.BLACK
+//        pieDataSet.valueTextSize = 16f
+//
+//        val pieData = PieData(pieDataSet)
+//
+////        pieChart.data = pieData
+////        pieChart.description.isEnabled = false
+////        pieChart.centerText = "COVID"
+////        pieChart.animate()
+//    }
 }
